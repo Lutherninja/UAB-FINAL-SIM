b9fd3a855dcb5a24f34b5dadead7eef8a3051078
Jonathan Luther <luther.ninja@gmail.com>
2020-02-24 13:31:29 -0700

    Project is in

---
+++ UAB VFX Project/Assets/Scripts/ParticleEffect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffect : MonoBehaviour
+{
+    public ParticleSystem correct;
+
+    public ParticleSystem incorrect;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        correct.Play();
+    }
+}
